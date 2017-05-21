adcffd5e6087756d8aa11124843ae39f2dc8485e
Ed Sadler <ed_lorie@comcast.net>
2017-05-21 18:21:08 -0500

    initial commit

---
+++ grades.js
@@ -0,0 +1,32 @@
+console.log("grades js is working");
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var gradeA = [];
+var gradeB = [];
+var gradeC = [];
+var gradeD = [];
+var gradeF = [];
+
+  for (var i = 0; i < scores.length; i++) {
+  // console.log(scores[i]);
+  if (scores[i] <= 100 && scores[i] > 91){
+    gradeA++;
+  } else if (scores[i] <= 90 && scores[i] > 81) {
+    gradeB++;
+  } else if (scores[i] <= 80 && scores[i] > 71) {
+    gradeC++;
+  } else if (scores[i] <= 70 && scores[i] > 61) {
+    gradeD++;
+  } else if (scores[i] <= 60 && scores[i] > 0) {
+    gradeF++;
+  }
+};
+console.log(gradeA, gradeB, gradeC, gradeD, gradeF);
+
+console.log("There are " + gradeA + " grade A's, " + gradeB + " grade B's, " + gradeC + " grade C's, " + gradeD + " grade D's, " + "and " + gradeF + " grade F's.");
+
+console.log("The lowest grade is " + Math.min.apply(null, scores));
+
+console.log("The highest grade is " + Math.max.apply(null, scores));
+
